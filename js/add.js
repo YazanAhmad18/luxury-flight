e66ffdeb001fc63f51283467a7eb226d4ae60062
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-03-25 15:11:41 +0200

    some edit

--- js/add.js
+++ js/add.js
@@ -1,18 +1,18 @@
 var name1 = prompt('what is your name');
-alert('welcome to our website'  +  name1);
+alert('welcome to our website' + "  " + name1);
 
 var airline= prompt('pleas tell us what is your favorite airline','Emirates-Airlines, Qatar-Airways, Turkey-Airlines');
 
 if(airline==='Emirates-Airlines'){
 
-document.write('<img src="js/download.jpg/" height="200px" width="200px">');
+document.write('<img src="download.jpg" height="200px" width="200px">');
  document.write('<p>  Emirates Airlines is an Emirati national airline, based at Dubai International Airport in the Emirate of Dubai, the second largest city in the United Arab Emirates, its headquarters and a center for its operations, and Emirates Airlines provides services to more than 120 destinations around the world </p>')
 
 
 }
 else if(airline==='Qatar-Airways') {
 
-    document.write('<img src="js/1.jpg/" height="200px" width="200px">');
+    document.write('<img src="1.jpg" height="200px" width="200px">');
      document.write('<p>  It is the national airline of the State of Qatar, and it is a state-owned company. The company operates a hub, connecting more than 150 international destinations </p>');
 
     
@@ -21,7 +21,7 @@ else if(airline==='Qatar-Airways') {
     }
     else if (airline==='Turkey-Airlines') {
 
-        document.write('<img src="/home/yazan/ASAC/luxury-flight/js/2.jpg" height="200px" width="200px">');
+        document.write('<img src="2.jpg" height="200px" width="200px">');
          document.write('<p>  It is a Turkish national airline, the main site of the company is in Istanbul, the largest of the Turkish cities, and takes Istanbul International Airport as a center for its operations. Turkish Airlines provides services to more than 315 destinations.</p>');
     
 
