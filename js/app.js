caac4e7a09e25f1c82d9908cc7a176b54d9d8b72
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-03-29 16:13:04 +0300

    some change

--- js/app.js
+++ js/app.js
@@ -1,54 +1,92 @@
 
 var age = prompt('what is your age');
 
-while (!(age >= 18 &&  age<100) ){
-alert("pleas the age you have been chose is not allowed your age should be more than or equal 18 and less 100 ")
-var age = prompt('what is your age ')
-}
+function userAge(userAge) {
 
-var name1 = prompt('what is your name');
-alert('welcome to our website' + "  " + name1);
 
- 
-var airline= prompt('pleas tell us what is your favorite airline','Emirates-Airlines, Qatar-Airways, Turkey-Airlines');
+    while (!(userAge >= 18 && userAge < 100)) {
+        alert("pleas the age you have been chose is not allowed your age should be more than or equal 18 and less 100 ")
+        var age = prompt('what is your age ')
 
-while(airline!=='Emirates-Airlines' && airline !=='Qatar-Airways' && airline !== 'Turkey-Airlines' ){
 
-var airline=prompt('you should only use one of these three airline Emirates-Airlines, Qatar-Airways, Turkey-Airlines')
-}
- 
-var airplaneImg = prompt('how many images do you want to see for this airline')
-
-var img= '';
-
-var txt= '';
-
-for(var i =1; i <= airplaneImg ; i++){
-
-    if(airline==='Emirates-Airlines' ){
-
-        img= img + '<img src="download.jpg" height="200px" width="200px">';
-         txt= '<p>Emirates Airlines is an Emirati national airline, based at Dubai International Airport in the Emirate of Dubai, the second largest city in the United Arab Emirates, its headquarters and a center for its operations, and Emirates Airlines provides services to more than 120 destinations around the world </p>';
-          
-        }
-        else if(airline==='Qatar-Airways') {
-        
-            img= img + '<img src="1.jpg" height="200px" width="200px">';
-             txt = '<p>  It is the national airline of the State of Qatar, and it is a state-owned company. The company operates a hub, connecting more than 150 international destinations </p>';
-            }
-       else if (airline==='Turkey-Airlines') {
-        
-                img= img +'<img src="2.jpg" height="200px" width="200px">';
-                txt ='<p>  It is a Turkish national airline, the main site of the company is in Istanbul, the largest of the Turkish cities, and takes Istanbul International Airport as a center for its operations. Turkish Airlines provides services to more than 315 destinations.</p>';
-        
-            }
-        else{
-        document.write('pleas choose only one of these airlines') 
     }
-    
+    return userAge;
+}
+userAge(age);
+
+
+
+var yearbirth = function (year) {
+
+    var birthdate = 2021 - age;
+    alert('you are birth year is ' + birthdate);
+    return birthdate;
+
 
 }
+yearbirth();
 
-console.log(img);
-document.write(img)
+function nameUser() {
+    var name1 = prompt('what is your name');
+    alert('welcome to our website' + "  " + name1);
+
+}
+nameUser();
+
+var airline ='';
+function choseAirline() {
+
+     airline = prompt('pleas tell us what is your favorite airline', 'Emirates-Airlines, Qatar-Airways, Turkey-Airlines');
+
+    while (airline !== 'Emirates-Airlines' && airline !== 'Qatar-Airways' && airline !== 'Turkey-Airlines') {
+         airline = prompt('you should only use one of these three airline Emirates-Airlines, Qatar-Airways, Turkey-Airlines')
+
+
+    }
+
+}
+choseAirline();
+
+var img1 = '';
+
+var txt = '';
+
+var airplaneImg ='';
+function img() {
+     airplaneImg = prompt('how many images do you want to see for this airline')
+
+    
+    for (var i = 1; i <= airplaneImg; i++) {
+
+        if (airline === 'Emirates-Airlines') {
+
+            img1 = img1 + '<img src="download.jpg" height="200px" width="200px">';
+            txt = '<p>Emirates Airlines is an Emirati national airline, based at Dubai International Airport in the Emirate of Dubai, the second largest city in the United Arab Emirates, its headquarters and a center for its operations, and Emirates Airlines provides services to more than 120 destinations around the world </p>';
+
+        }
+        else if (airline === 'Qatar-Airways') {
+
+            img1 = img1 + '<img src="1.jpg" height="200px" width="200px">';
+            txt = '<p>  It is the national airline of the State of Qatar, and it is a state-owned company. The company operates a hub, connecting more than 150 international destinations </p>';
+        }
+        else if (airline === 'Turkey-Airlines') {
+
+            img1 = img1 + '<img src="2.jpg" height="200px" width="200px">';
+            txt = '<p>  It is a Turkish national airline, the main site of the company is in Istanbul, the largest of the Turkish cities, and takes Istanbul International Airport as a center for its operations. Turkish Airlines provides services to more than 315 destinations.</p>';
+
+        }
+        else {
+            document.write('pleas choose only one of these airlines')
+        }
+
+
+    }
+}
+
+img();
+
+
+
+console.log(img1);
+document.write(img1)
 document.write(txt)
